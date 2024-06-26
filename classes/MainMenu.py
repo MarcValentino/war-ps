2b9e255203e62d41726fe1b206c090863de726f5
Henrique <hiquemar@hotmail.com>
2024-06-26 10:30:39 -0300

    feat(menu): add options menu

--- classes/MainMenu.py
+++ classes/MainMenu.py
@@ -1,51 +1,55 @@
+import pygame
 from classes.Button import MenuButton
 from classes.Game import *
+from classes.OptionsMenu import OptionsMenu
 
 PLAY_BUTTON = "Jogar"
 OPTIONS_BUTTON = "Opções"
 QUIT_BUTTON = "Fechar"
 
+
 class MainMenu:
     def __init__(self):
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((800, 600))
         self.buttons = [
             MenuButton(PLAY_BUTTON, 200),
             MenuButton(OPTIONS_BUTTON, 300),
             MenuButton(QUIT_BUTTON, 400)
         ]
         self.background_image = pygame.image.load('classes/assets/images/bg/main-menu-bg.png')
+        self.in_options_menu = False
+        self.options_menu = OptionsMenu(self.exit_options_menu)
 
-    def show_options_menu(self):
-        print("Abrindo menu de opções...")
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    running = False
-            self.screen.fill(WHITE)
-            pygame.display.flip()
+    def exit_options_menu(self):
+        self.in_options_menu = False
 
     def run(self):
         running = True
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                for button in self.buttons:
-                    if button.is_clicked(event):
-                        if button.text == PLAY_BUTTON:
-                            game = Game()
-                            game.onExecute()
-                        elif button.text == OPTIONS_BUTTON:
-                            self.show_options_menu()
-                        elif button.text == QUIT_BUTTON:
-                            running = False
+                elif self.in_options_menu:
+                    self.options_menu.handle_event(event)
+                else:
+                    for button in self.buttons:
+                        if button.is_clicked(event):
+                            if button.text == PLAY_BUTTON:
+                                game = Game()
+                                game.onExecute()
+                            elif button.text == OPTIONS_BUTTON:
+                                self.in_options_menu = True
+                            elif button.text == QUIT_BUTTON:
+                                running = False
 
-            self.screen.blit(self.background_image, (0, 0))
-            for button in self.buttons:
-                button.draw(self.screen)
+            if self.in_options_menu:
+                self.options_menu.draw()
+            else:
+                self.screen.blit(self.background_image, (0, 0))
+                for button in self.buttons:
+                    button.draw(self.screen)
+
             pygame.display.flip()
 
         pygame.quit()
